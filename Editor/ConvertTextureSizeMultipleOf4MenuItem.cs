fdc3d8a3c9a95f7a851fdf1146e18f27d50a6d21
baba-s <shotababa1206@gmail.com>
2022-09-30 11:31:30 +0900

    add

---
+++ Editor/ConvertTextureSizeMultipleOf4MenuItem.cs
@@ -0,0 +1,94 @@
+using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+    internal static class ConvertTextureSizeMultipleOf4
+    {
+        private const string MENU_ITEM_NAME = "Assets/Kogane/Convert Texture Size Multiple Of 4";
+
+        [MenuItem( MENU_ITEM_NAME, true, 1154515621 )]
+        private static bool CanConvert()
+        {
+            return Selection.objects.OfType<Texture2D>().Any();
+        }
+
+        [MenuItem( MENU_ITEM_NAME, false )]
+        private static void Convert()
+        {
+            if ( !EditorUtility.DisplayDialog( "", "選択中のすべてのテクスチャのサイズを 4 の倍数に変換しますか？", "はい", "いいえ" ) ) return;
+
+            var textureArray = Selection.objects.OfType<Texture2D>().ToArray();
+
+            try
+            {
+                AssetDatabase.StartAssetEditing();
+
+                var length = textureArray.Length;
+
+                for ( var i = 0; i < length; i++ )
+                {
+                    var number     = i + 1;
+                    var oldTexture = textureArray[ i ];
+
+                    EditorUtility.DisplayProgressBar
+                    (
+                        title: "Convert Texture Size Multiple Of 4",
+                        info: $"{number} / {length}    {oldTexture.name}",
+                        progress: ( float )number / length
+                    );
+
+                    var oldWidth  = oldTexture.width;
+                    var oldHeight = oldTexture.height;
+                    var newWidth  = ( int )RoundByMultiple( oldWidth, 4 );
+                    var newHeight = ( int )RoundByMultiple( oldHeight, 4 );
+
+                    if ( oldWidth == newWidth && oldHeight == newHeight ) continue;
+
+                    var newTexture = ResizeTexture( oldTexture, newWidth, newHeight );
+                    var assetPath  = AssetDatabase.GetAssetPath( oldTexture );
+                    var bytes      = newTexture.EncodeToPNG();
+
+                    File.WriteAllBytes( assetPath, bytes );
+                }
+            }
+            finally
+            {
+                EditorUtility.ClearProgressBar();
+                AssetDatabase.StopAssetEditing();
+            }
+
+            AssetDatabase.Refresh();
+
+            EditorUtility.DisplayDialog( "", "選択中のすべてのテクスチャのサイズを 4 の倍数に変換しました", "OK" );
+        }
+
+        private static double RoundByMultiple( double value, double unit )
+        {
+            return Math.Round( value / unit ) * unit;
+        }
+
+        private static Texture2D ResizeTexture( Texture texture, int width, int height )
+        {
+            var temporaryRenderTexture = RenderTexture.GetTemporary( width, height );
+
+            Graphics.Blit( texture, temporaryRenderTexture );
+
+            var oldRenderTexture = RenderTexture.active;
+
+            RenderTexture.active = temporaryRenderTexture;
+
+            var newTexture = new Texture2D( width, height );
+            newTexture.ReadPixels( new Rect( 0, 0, width, height ), 0, 0 );
+            newTexture.Apply();
+
+            RenderTexture.active = oldRenderTexture;
+            RenderTexture.ReleaseTemporary( temporaryRenderTexture );
+
+            return newTexture;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.ConvertTextureSizeMultipleOf4
+# Kogane Convert Texture Size Multiple Of4 Menu Item
+
+選択中のすべてのテクスチャのサイズを 4 の倍数に変換する MenuItem を追加するエディタ拡張
